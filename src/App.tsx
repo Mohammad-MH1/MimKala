e783b1b6abf524fa1e83afcd17f4c0ff904518bf
Mohammad Mohammadi <Mohammadyahoo15@gmail.com>
2024-10-01 15:20:57 +0330

    Create page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './ui/AppLayout';
+import HomePage from './pages/HomePage';
+import Product from './pages/Product';
+
 function App() {
-  return <div>MimKala</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path='product/:id' element={<Product />} />
+        </Route>
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
