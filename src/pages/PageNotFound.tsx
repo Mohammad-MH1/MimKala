d2d53ad4e4d550a17f4e608923cc3a984dc13fd6
Mohammad Mohammadi <Mohammadyahoo15@gmail.com>
2024-10-04 20:24:00 +0330

    Change website title and Add some basic style to PageNotFound

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,5 +1,11 @@
 function PageNotFound() {
-  return <div>Not found</div>;
+  return (
+    <div className='flex h-dvh w-full items-center justify-center'>
+      <p className='rounded-full bg-blue-400 p-10 text-3xl'>
+        404 Page Not Found
+      </p>
+    </div>
+  );
 }
 
 export default PageNotFound;
